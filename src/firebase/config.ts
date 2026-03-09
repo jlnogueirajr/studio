386a991486d41032c1652b5411dafbb6a51d01b1
Jose Luis Nogueira Jr <joseluisnogueirajr@gmail.com>
2026-03-09 23:51:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4036367494-bc359",
+  "appId": "1:143368459654:web:5e8da1da6ed060a7570807",
+  "apiKey": "AIzaSyCP3Pjz5HdKdvrR1D6eEq9s7GXB7uqEPZI",
+  "authDomain": "studio-4036367494-bc359.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "143368459654"
+};
